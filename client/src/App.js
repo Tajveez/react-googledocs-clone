110931622d0de30163e5d3900ac3794cc8ef61ed
tajveez <tajveez786@gmail.com>
2021-05-03 23:50:30 +0500

    Dependencies added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-import TextEditor from './components/TextEditor'
+import TextEditor from "./components/TextEditor";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 // main componenet
 function App() {
   return (
