46d3d7d7d6a90dc5cd9561b2b85f749cd3f3691c
tajveez <tajveez786@gmail.com>
2021-05-04 22:50:32 +0500

    MongoDB functions done

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Document = require('./Document')
+
 mongoose.connect('mongodb://localhost/google-docs-clone', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -14,15 +16,26 @@ const io = require('socket.io')(3001, {
 });
 
 io.on('connection', socket => {
-    socket.on('get-document', documentId => {
-        const data = ""
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
         socket.join(documentId)
-        socket.emit('load-document', data)
+        socket.emit('load-document', document.data)
         socket.on('send-changes', delta => {
             // console.log(delta)
             socket.broadcast.to(documentId).emit('receive-changes', delta)
+        })
+
+        socket.on('save-document', async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
         })
     })
 
     console.log('connected!')
 })
+
+async function findOrCreateDocument(id) {
+    if (id == null) return
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({ id, data: "" })
+}
