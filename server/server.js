2e2c87a967d42e68febd749cb55a0fdcd9852601
tajveez <tajveez786@gmail.com>
2021-05-04 22:42:55 +0500

    Mongoose installed

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/google-docs-clone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
